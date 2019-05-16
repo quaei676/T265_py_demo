7706eb2cfa643255514f23a01d276f7657920698
quaei676 <39039034+quaei676@users.noreply.github.com>
2019-05-16 15:23:25 +0800

    using camera intrinsics

--- T265-defish.py
+++ T265-defish.py
@@ -3,7 +3,30 @@ import pyrealsense2 as rs
 from pprint import pprint
 import numpy as np
 import cv2
+from math import tan, pi
 
+"""
+Returns R, T transform from src to dst
+"""
+def get_extrinsics(src, dst):
+    extrinsics = src.get_extrinsics_to(dst)
+    R = np.reshape(extrinsics.rotation, [3,3]).T
+    T = np.array(extrinsics.translation)
+    return (R, T)
+
+"""
+Returns a camera matrix K from librealsense intrinsics
+"""
+def camera_matrix(intrinsics):
+    return np.array([[intrinsics.fx,             0, intrinsics.ppx],
+                     [            0, intrinsics.fy, intrinsics.ppy],
+                     [            0,             0,              1]])
+
+"""
+Returns the fisheye distortion from librealsense intrinsics
+"""
+def fisheye_distortion(intrinsics):
+    return np.array(intrinsics.coeffs[:4])
 # Get realsense pipeline
 pipe = rs.pipeline()
 
@@ -21,32 +44,67 @@ pprint(dir(rs.stream))
 pipe.start(cfg)
 
 try:
-    for _ in range(10000):
+    min_disp = 0
+    # must be divisible by 16
+    num_disp = 160 - min_disp
+    max_disp = min_disp + num_disp
+    profiles = pipe.get_active_profile()
+    streams = {"left"  : profiles.get_stream(rs.stream.fisheye, 1).as_video_stream_profile(),
+               "right" : profiles.get_stream(rs.stream.fisheye, 2).as_video_stream_profile()}
+    intrinsics = {"left"  : streams["left"].get_intrinsics(),
+                  "right" : streams["right"].get_intrinsics()}
+    # Translate the intrinsics from librealsense into OpenCV
+    K_left  = camera_matrix(intrinsics["left"])
+    D_left  = fisheye_distortion(intrinsics["left"])
+    K_right = camera_matrix(intrinsics["right"])
+    D_right = fisheye_distortion(intrinsics["right"])
+    (width, height) = (intrinsics["left"].width, intrinsics["left"].height)
+    # Get the relative extrinsics between the left and right camera
+    (R, T) = get_extrinsics(streams["left"], streams["right"])
+    stereo_fov_rad = 90 * (pi/180)  # 90 degree desired fov
+    stereo_height_px = 700          # 300x300 pixel stereo output
+    stereo_focal_px = stereo_height_px/2 / tan(stereo_fov_rad/2)
+    # We set the left rotation to identity and the right rotation
+    # the rotation between the cameras
+    R_left = np.eye(3)
+    R_right = R
+    # The stereo algorithm needs max_disp extra pixels in order to produce valid
+    # disparity on the desired output region. This changes the width, but the
+    # center of projection should be on the center of the cropped image
+    stereo_width_px = stereo_height_px + max_disp
+    stereo_size = (stereo_width_px, stereo_height_px)
+    stereo_cx = (stereo_height_px - 1)/2 + max_disp
+    stereo_cy = (stereo_height_px - 1)/2
+    P_left = np.array([[stereo_focal_px, 0, stereo_cx, 0],
+                       [0, stereo_focal_px, stereo_cy, 0],
+                       [0,               0,         1, 0]])
+    P_right = P_left.copy()
+    P_right[0][3] = T[0]*stereo_focal_px
+    # Construct Q for use with cv2.reprojectImageTo3D. Subtract max_disp from x
+    # since we will crop the disparity later
+    Q = np.array([[1, 0,       0, -(stereo_cx - max_disp)],
+                  [0, 1,       0, -stereo_cy],
+                  [0, 0,       0, stereo_focal_px],
+                  [0, 0, -1/T[0], 0]])
+    (w, h) = (intrinsics["left"].width, intrinsics["left"].height)
+    mapx_l,mapy_l = cv2.fisheye.initUndistortRectifyMap(K_left,D_left,R_left,P_left,(w,h),cv2.CV_32FC1)
+    mapx_r,mapy_r = cv2.fisheye.initUndistortRectifyMap(K_right, D_right, R_right, P_right,(w,h),cv2.CV_32FC1)
+    while True:
         frames = pipe.wait_for_frames()
-
         left = frames.get_fisheye_frame(1)
         left_data = np.asanyarray(left.get_data())
 
         right = frames.get_fisheye_frame(2)
         right_data = np.asanyarray(right.get_data())
-        h,  w = left_data.shape[:2]
-        mtx=np.array([[285.629349, 0.000000, 421.755510],[0.000000, 287.216162, 387.112779],[0.000000, 0.000000, 1.000000]])
-        dist=np.array([-0.232094, 0.036002, 0.003086, 0.003522, 0.000000])
-        rec=np.array([[0.987407, 0.025541, 0.156124],[0.021770, 0.999430, 0.025817],[0.156694, 0.022093, 0.987400]])
-        proj=np.array([[297.923209, 0.000000, 412.454276, 0.000000],[0.000000, 297.923209, 398.625719, 0.000000],[ 0.000000,0.000000, 1.000000, 0.000000]])
-        mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,np.eye(3),proj,(w,h),cv2.CV_32FC1)
-        dst_left = cv2.remap(left_data,mapx,mapy,cv2.INTER_LINEAR,borderMode=cv2.BORDER_CONSTANT)
 
-        mtx1=np.array([[265.879331, 0.000000, 421.388842],[ 0.000000, 267.722657, 404.000467],[0.000000, 0.000000, 1.000000]])
-        dist1=np.array([-0.201737, 0.028530, -0.002716, 0.000891, 0.000000])
-        rec1=np.array([[0.993738, 0.020699, 0.109798, -0.023336, 0.999468, 0.022783, -0.109268, -0.025203, 0.993693]])
-        proj1=np.array([[297.923209, 0.000000, 412.454276, -196.472363],[0.000000, 297.923209, 398.625719, 0.000000],[ 0.000000,0.000000, 1.000000, 0.000000]])
-        mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,np.eye(3),proj,(w,h),cv2.CV_32FC1)
-        dst_right = cv2.remap(right_data,mapx,mapy,cv2.INTER_LINEAR,borderMode=cv2.BORDER_CONSTANT)
+        dst_left = cv2.remap(left_data,mapx_l,mapy_l,cv2.INTER_LINEAR,borderMode=cv2.BORDER_CONSTANT)
+
+        dst_right = cv2.remap(right_data,mapx_r,mapy_r,cv2.INTER_LINEAR,borderMode=cv2.BORDER_CONSTANT)
 
 
-        # show the calibrated image
+        # crop the image
 
+        #cv2.imwrite('calibresult.png',dst)
         cv2.imshow('left', dst_left)
         cv2.imshow('left_ori', left_data)
         cv2.imshow('right', dst_right)
